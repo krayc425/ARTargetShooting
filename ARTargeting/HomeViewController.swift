06437b70c2c2f9297ac9d39a9dfe187fad619e02
Kuixi Song <krayc425@gmail.com>
2018-06-04 23:31:36 -0700

    Add HomeVC in App version

---
+++ ARTargeting/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  ARTargeting
+//
+//  Created by 宋 奎熹 on 2018/6/4.
+//  Copyright © 2018 宋 奎熹. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ARTargeting/SingleViewController.swift
+++ ARTargeting/SingleViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class SingleViewController: UIViewController, ARSCNViewDelegate {
 
     private let generationCycle     : TimeInterval   = 3.0
     
@@ -61,7 +61,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let scene = SCNScene()
         sceneView.scene = scene
         
-        sceneView.scene.physicsWorld.contactDelegate = self
         sceneView.scene.physicsWorld.gravity = SCNVector3(0, -1, 0)
     }
     
@@ -104,10 +103,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         generateTimer.invalidate()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
     private func generateTarget() {
         let x: Float = Float(arc4random() % 4) - 2.0
         let y: Float = Float(arc4random() % 2) + 0.5
@@ -123,18 +118,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         playSound(.appear)
     }
-    
     private func generateSmallTarget(oldTarget: TargetNode) {
         let oldPosition = oldTarget.presentation.position
         
         for i in 0...1 {
             let targetNode = TargetNode.generateSmallTarget(oldTarget: oldTarget)
             targetNode.position = oldPosition
-            
             targetNode.rotation = SCNVector4(x: 1, y: 0, z: 0, w: .pi / 2.0)
             
             self.targetNodes.insert(targetNode)
-            targetNode.physicsBody?.applyForce(SCNVector3((0.5 - Double(i)) * 0.6, drand48() - 0.5, 0.0), asImpulse: true)
+            targetNode.physicsBody?.applyForce(SCNVector3((0.5 - Double(i)) * drand48(),
+                                                          drand48() - 0.5,
+                                                          drand48() - 0.5), asImpulse: true)
             self.sceneView.scene.rootNode.addChildNode(targetNode)
         }
     }
@@ -145,18 +140,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         
         playSound(.shoot)
-        
         let (direction, position) = getUserVector(in: self.sceneView.session.currentFrame)
         
         var endVector = position + direction * 10
         
         for targetNode in targetNodes {
             let targetVector = targetNode.presentation.position + position * (-1)
-            
-            let targetDistance = targetNode.presentation.position.distance(from: position)
-            
-            if !targetNode.hit && fabs(targetVector.theta(from: direction)) <= fabs(atan(Float(targetNode.radius) / targetDistance)) {
-                
+            if !targetNode.hit && fabs(targetVector.theta(from: direction)) <= fabs(atan(Float(targetNode.radius) / targetNode.presentation.position.distance(from: position))) {
                 currentScore += targetNode.hitScore
                 self.generateSmallTarget(oldTarget: targetNode)
                 playSound(.hit)
@@ -170,7 +160,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 endVector = targetNode.presentation.position
                 
                 targetNode.hit = true
-
                 break
             }
         }
@@ -205,14 +194,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
-    
-}
-
-extension ViewController: SCNPhysicsContactDelegate {
-    
-    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
         
     }
     
